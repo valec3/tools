95d9a59248df787bb8ce0bc550898b4b838e526f
valec3 <v1c70rotakmer@gmail.com>
2025-11-27 21:14:36 -0500

    feat: Implement a file upload server with Express, Multer, and file management endpoints.

--- upload-app/server.js
+++ upload-app/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors(
     {
-        origin: ['https://quierolapromocion.com', 'http://luxurystarspa.com', 'http://localhost:3000'],
+        origin: "*",
         credentials: true   
     }
 ));
